1901b40f54a9c40c8bcbf1912a085b393a6f92d5
A$AP Lucky <maciagmarzello@gmail.com>
2023-05-04 12:35:05 +0100

    Created a Main_Form layout

---
+++ BL/Repository/DataFacade.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL.Repository
+{
+    internal class DataFacade
+    {
+        DAL.Linq2SQL.ITExpressDataClassesDataContext ITExpressDataContext { get; set; }
+
+
+    }
+}
--- UI/All/Main_Form.cs
+++ UI/All/Main_Form.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,11 +11,12 @@ using System.Windows.Forms;
 
 namespace UI
 {
-    public partial class Form1 : Form
+    public partial class Main_Form : Form
     {
-        public Form1()
+        public Main_Form()
         {
             InitializeComponent();
+
         }
     }
 }
