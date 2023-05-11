bda689c4e10302be9f8e6462db3d723ba2660c98
A$AP Lucky <maciagmarzello@gmail.com>
2023-05-11 12:32:26 +0100

    stuff
    stuff

--- Presentation/Customer/InvitationSend_Form.Designer.cs
+++ Presentation/Customer/InvitationSend_Form.Designer.cs
@@ -38,6 +38,14 @@
             this.panel2 = new System.Windows.Forms.Panel();
             this.label1 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.dtp_endDate = new System.Windows.Forms.DateTimePicker();
+            this.dtp_startDate = new System.Windows.Forms.DateTimePicker();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.bt_DiscardProject = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
             this.SuspendLayout();
@@ -45,6 +53,12 @@
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Controls.Add(this.dtp_endDate);
+            this.panel1.Controls.Add(this.dtp_startDate);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.label7);
+            this.panel1.Controls.Add(this.textBox5);
+            this.panel1.Controls.Add(this.label5);
             this.panel1.Controls.Add(this.textBox4);
             this.panel1.Controls.Add(this.label4);
             this.panel1.Controls.Add(this.textBox3);
@@ -53,12 +67,12 @@
             this.panel1.Controls.Add(this.label2);
             this.panel1.Location = new System.Drawing.Point(13, 13);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(557, 128);
+            this.panel1.Size = new System.Drawing.Size(773, 250);
             this.panel1.TabIndex = 0;
             // 
             // textBox4
             // 
-            this.textBox4.Location = new System.Drawing.Point(47, 95);
+            this.textBox4.Location = new System.Drawing.Point(234, 94);
             this.textBox4.Name = "textBox4";
             this.textBox4.Size = new System.Drawing.Size(501, 20);
             this.textBox4.TabIndex = 7;
@@ -67,15 +81,15 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
-            this.label4.Location = new System.Drawing.Point(6, 95);
+            this.label4.Location = new System.Drawing.Point(10, 95);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(36, 15);
+            this.label4.Size = new System.Drawing.Size(70, 15);
             this.label4.TabIndex = 6;
-            this.label4.Text = "From";
+            this.label4.Text = "Hour wage:";
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(48, 54);
+            this.textBox3.Location = new System.Drawing.Point(234, 54);
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(501, 20);
             this.textBox3.TabIndex = 5;
@@ -84,15 +98,15 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
-            this.label3.Location = new System.Drawing.Point(7, 54);
+            this.label3.Location = new System.Drawing.Point(10, 54);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(21, 15);
+            this.label3.Size = new System.Drawing.Size(131, 15);
             this.label3.TabIndex = 4;
-            this.label3.Text = "To";
+            this.label3.Text = "Project requierements:";
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(45, 13);
+            this.textBox2.Location = new System.Drawing.Point(234, 13);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(501, 20);
             this.textBox2.TabIndex = 3;
@@ -101,20 +115,20 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
-            this.label2.Location = new System.Drawing.Point(4, 13);
+            this.label2.Location = new System.Drawing.Point(10, 14);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(30, 15);
+            this.label2.Size = new System.Drawing.Size(70, 15);
             this.label2.TabIndex = 2;
-            this.label2.Text = "Title";
+            this.label2.Text = "Project title:";
             // 
             // panel2
             // 
             this.panel2.BackColor = System.Drawing.Color.White;
             this.panel2.Controls.Add(this.label1);
             this.panel2.Controls.Add(this.textBox1);
-            this.panel2.Location = new System.Drawing.Point(13, 147);
+            this.panel2.Location = new System.Drawing.Point(13, 291);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(557, 291);
+            this.panel2.Size = new System.Drawing.Size(773, 118);
             this.panel2.TabIndex = 1;
             // 
             // label1
@@ -123,30 +137,116 @@
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
             this.label1.Location = new System.Drawing.Point(6, 6);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(34, 15);
+            this.label1.Size = new System.Drawing.Size(111, 15);
             this.label1.TabIndex = 1;
-            this.label1.Text = "Body";
+            this.label1.Text = "Project description:";
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(9, 24);
+            this.textBox1.Location = new System.Drawing.Point(13, 24);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(540, 258);
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(748, 82);
             this.textBox1.TabIndex = 0;
             // 
-            // Message_Form
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.label5.Location = new System.Drawing.Point(10, 134);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(39, 15);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "From:";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(234, 129);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(501, 20);
+            this.textBox5.TabIndex = 9;
+            // 
+            // dtp_endDate
+            // 
+            this.dtp_endDate.Location = new System.Drawing.Point(234, 219);
+            this.dtp_endDate.Name = "dtp_endDate";
+            this.dtp_endDate.Size = new System.Drawing.Size(215, 20);
+            this.dtp_endDate.TabIndex = 32;
+            // 
+            // dtp_startDate
+            // 
+            this.dtp_startDate.Location = new System.Drawing.Point(234, 179);
+            this.dtp_startDate.Name = "dtp_startDate";
+            this.dtp_startDate.Size = new System.Drawing.Size(215, 20);
+            this.dtp_startDate.TabIndex = 31;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.label6.Location = new System.Drawing.Point(10, 219);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(59, 15);
+            this.label6.TabIndex = 30;
+            this.label6.Text = "End date:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.label7.Location = new System.Drawing.Point(10, 179);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(62, 15);
+            this.label7.TabIndex = 29;
+            this.label7.Text = "Start date:";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(200)))), ((int)(((byte)(56)))));
+            this.button1.CausesValidation = false;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.button1.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.button1.Location = new System.Drawing.Point(594, 437);
+            this.button1.Margin = new System.Windows.Forms.Padding(0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(195, 28);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Accept project";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // bt_DiscardProject
+            // 
+            this.bt_DiscardProject.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(196)))), ((int)(((byte)(45)))), ((int)(((byte)(56)))));
+            this.bt_DiscardProject.CausesValidation = false;
+            this.bt_DiscardProject.FlatAppearance.BorderSize = 0;
+            this.bt_DiscardProject.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.bt_DiscardProject.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.bt_DiscardProject.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.bt_DiscardProject.Location = new System.Drawing.Point(13, 437);
+            this.bt_DiscardProject.Margin = new System.Windows.Forms.Padding(0);
+            this.bt_DiscardProject.Name = "bt_DiscardProject";
+            this.bt_DiscardProject.Size = new System.Drawing.Size(195, 28);
+            this.bt_DiscardProject.TabIndex = 4;
+            this.bt_DiscardProject.Text = "Decline invitation";
+            this.bt_DiscardProject.UseVisualStyleBackColor = false;
+            // 
+            // InvitationSend_Form
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(91)))), ((int)(((byte)(92)))), ((int)(((byte)(112)))));
-            this.ClientSize = new System.Drawing.Size(583, 450);
+            this.ClientSize = new System.Drawing.Size(798, 479);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.bt_DiscardProject);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
-            this.MaximumSize = new System.Drawing.Size(599, 489);
-            this.MinimumSize = new System.Drawing.Size(599, 489);
-            this.Name = "Message_Form";
-            this.Text = "Message";
+            this.MaximumSize = new System.Drawing.Size(814, 518);
+            this.MinimumSize = new System.Drawing.Size(814, 518);
+            this.Name = "InvitationSend_Form";
+            this.Text = "Invitation";
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
             this.panel2.ResumeLayout(false);
@@ -167,5 +267,13 @@
         private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.DateTimePicker dtp_endDate;
+        private System.Windows.Forms.DateTimePicker dtp_startDate;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button bt_DiscardProject;
     }
 }
