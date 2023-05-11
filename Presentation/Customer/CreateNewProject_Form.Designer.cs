edadcffd3e12a33baac414d5b2742bf679ba8842
A$AP Lucky <maciagmarzello@gmail.com>
2023-05-11 08:31:09 +0100

    Button change
    title

--- Presentation/Customer/CreateNewProject_Form.Designer.cs
+++ Presentation/Customer/CreateNewProject_Form.Designer.cs
@@ -47,6 +47,8 @@
             this.bt_DiscardProject = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
             this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
             this.SuspendLayout();
@@ -54,6 +56,8 @@
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.label7);
             this.panel1.Controls.Add(this.dtp_endDate);
             this.panel1.Controls.Add(this.dtp_startDate);
             this.panel1.Controls.Add(this.label6);
@@ -106,7 +110,7 @@
             // 
             // tb_projectHourWage
             // 
-            this.tb_projectHourWage.Location = new System.Drawing.Point(199, 206);
+            this.tb_projectHourWage.Location = new System.Drawing.Point(199, 268);
             this.tb_projectHourWage.Name = "tb_projectHourWage";
             this.tb_projectHourWage.Size = new System.Drawing.Size(278, 20);
             this.tb_projectHourWage.TabIndex = 22;
@@ -116,7 +120,7 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
-            this.label4.Location = new System.Drawing.Point(11, 206);
+            this.label4.Location = new System.Drawing.Point(11, 268);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(90, 20);
             this.label4.TabIndex = 21;
@@ -231,13 +235,31 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(195, 28);
             this.button1.TabIndex = 3;
-            this.button1.Text = "Discard project details";
+            this.button1.Text = "Create project";
             this.button1.UseVisualStyleBackColor = false;
             // 
             // contextMenuStrip1
             // 
             this.contextMenuStrip1.Name = "contextMenuStrip1";
             this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(200, 209);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(278, 20);
+            this.textBox1.TabIndex = 30;
+            this.textBox1.Text = "Coding language requierements";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label7.Location = new System.Drawing.Point(12, 207);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(76, 20);
+            this.label7.TabIndex = 29;
+            this.label7.Text = "End type:";
             // 
             // CreateNewProject_Form
             // 
@@ -282,5 +304,7 @@
         private System.Windows.Forms.DateTimePicker dtp_endDate;
         private System.Windows.Forms.DateTimePicker dtp_startDate;
         private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label7;
     }
 }
