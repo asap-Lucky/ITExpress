548fd2cd89ed172bf5e99b3664fc52bb1918db46
A$AP Lucky <maciagmarzello@gmail.com>
2023-05-11 10:49:38 +0100

    Things
    title

--- Presentation/Customer/MessageBoxCustumer_Form.Designer.cs
+++ Presentation/Customer/MessageBoxCustumer_Form.Designer.cs
@@ -33,10 +33,11 @@
             this.bt_deleteMessage = new System.Windows.Forms.Button();
             this.dgv_newMessages = new System.Windows.Forms.DataGridView();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.panel2 = new System.Windows.Forms.Panel();
-            this.dgv_currentConversations = new System.Windows.Forms.DataGridView();
             this.lb_LogInSign = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
             this.label1 = new System.Windows.Forms.Label();
+            this.dgv_currentConversations = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dgv_newMessages)).BeginInit();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
@@ -51,7 +52,7 @@
             this.bt_openMessage.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.bt_openMessage.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.bt_openMessage.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.bt_openMessage.Location = new System.Drawing.Point(33, 266);
+            this.bt_openMessage.Location = new System.Drawing.Point(33, 233);
             this.bt_openMessage.Name = "bt_openMessage";
             this.bt_openMessage.Size = new System.Drawing.Size(205, 30);
             this.bt_openMessage.TabIndex = 20;
@@ -81,7 +82,7 @@
             this.bt_deleteMessage.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.bt_deleteMessage.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.bt_deleteMessage.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.bt_deleteMessage.Location = new System.Drawing.Point(33, 398);
+            this.bt_deleteMessage.Location = new System.Drawing.Point(33, 319);
             this.bt_deleteMessage.Name = "bt_deleteMessage";
             this.bt_deleteMessage.Size = new System.Drawing.Size(205, 30);
             this.bt_deleteMessage.TabIndex = 22;
@@ -107,25 +108,6 @@
             this.panel1.Size = new System.Drawing.Size(359, 533);
             this.panel1.TabIndex = 24;
             // 
-            // panel2
-            // 
-            this.panel2.BackColor = System.Drawing.Color.White;
-            this.panel2.Controls.Add(this.label1);
-            this.panel2.Controls.Add(this.dgv_currentConversations);
-            this.panel2.Location = new System.Drawing.Point(709, 12);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(359, 533);
-            this.panel2.TabIndex = 25;
-            // 
-            // dgv_currentConversations
-            // 
-            this.dgv_currentConversations.BackgroundColor = System.Drawing.Color.White;
-            this.dgv_currentConversations.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgv_currentConversations.Location = new System.Drawing.Point(0, 73);
-            this.dgv_currentConversations.Name = "dgv_currentConversations";
-            this.dgv_currentConversations.Size = new System.Drawing.Size(359, 460);
-            this.dgv_currentConversations.TabIndex = 24;
-            // 
             // lb_LogInSign
             // 
             this.lb_LogInSign.AutoSize = true;
@@ -138,6 +120,16 @@
             this.lb_LogInSign.TabIndex = 24;
             this.lb_LogInSign.Text = "New messages";
             this.lb_LogInSign.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.White;
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.dgv_currentConversations);
+            this.panel2.Location = new System.Drawing.Point(709, 12);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(359, 533);
+            this.panel2.TabIndex = 25;
             // 
             // label1
             // 
@@ -152,6 +144,30 @@
             this.label1.Text = "Current conversations";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
+            // dgv_currentConversations
+            // 
+            this.dgv_currentConversations.BackgroundColor = System.Drawing.Color.White;
+            this.dgv_currentConversations.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv_currentConversations.Location = new System.Drawing.Point(0, 73);
+            this.dgv_currentConversations.Name = "dgv_currentConversations";
+            this.dgv_currentConversations.Size = new System.Drawing.Size(359, 460);
+            this.dgv_currentConversations.TabIndex = 24;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(177)))), ((int)(((byte)(136)))));
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button1.Location = new System.Drawing.Point(33, 414);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(205, 30);
+            this.button1.TabIndex = 26;
+            this.button1.Text = "Refresh";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
             // MessageBoxCustumer
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -159,6 +175,7 @@
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(91)))), ((int)(((byte)(92)))), ((int)(((byte)(112)))));
             this.ClientSize = new System.Drawing.Size(1080, 557);
             this.ControlBox = false;
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.bt_deleteMessage);
@@ -188,5 +205,6 @@
         private System.Windows.Forms.DataGridView dgv_currentConversations;
         private System.Windows.Forms.Label lb_LogInSign;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
     }
 }
