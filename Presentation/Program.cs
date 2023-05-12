7e4e3e616b4e5b39b4bab82f227ee088c146cd94
A$AP Lucky <maciagmarzello@gmail.com>
2023-05-12 09:01:09 +0100

    Small changes
    title

--- Presentation/Program.cs
+++ Presentation/Program.cs
@@ -23,7 +23,7 @@ namespace UI
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Main_Form());
+            Application.Run(new ConsultantOverviewWindow_Form());
         }
     }
 }
