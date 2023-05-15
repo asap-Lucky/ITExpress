430fe76cdb894b6908912947c995179ce34ec381
Djoan Golovko <djoa0002@gmail.com>
2023-05-15 11:10:58 +0200

    Added further support for Specialization class, including a service and an interface for it

---
+++ BLL/Services/SpecializationService.cs
@@ -0,0 +1,40 @@
+﻿using Abstraction.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Services
+{
+    public class SpecializationService : ISpecializationService
+    {
+        ISpecializationRepo _SpecializationRepo;
+
+        public SpecializationService()
+        {
+            _SpecializationRepo = new DAL.Repository.SpecializationRepo();
+        }
+
+        public void AddSpecialization(ISpecialization specialization)
+        {
+            _SpecializationRepo.AddSpecialization(specialization);
+        }
+
+        public void DeleteSpecialization(ISpecialization specialization)
+        {
+            _SpecializationRepo.DeleteSpecialization(specialization);
+        }
+
+        public List<ISpecialization> GetAllSpecializations()
+        {
+            return _SpecializationRepo.GetAllSpecialization();
+        }
+
+        public void EditSpecialization(ISpecialization specialization)
+        {
+            _SpecializationRepo.EditSpecialization(specialization);
+        }
+
+    }
+}
