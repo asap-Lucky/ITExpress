6b0d150671c7deef4e9c12ef77057cac40e28bb1
Djoan Golovko <djoa0002@gmail.com>
2023-05-16 11:41:13 +0200

    CodeLanguage
    Added repository and service layer for code language

---
+++ Abstraction/Interfaces/ICodeLanguageRepo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstraction.Interfaces
+{
+    public interface ICodeLanguageRepo
+    {
+        List<ICodeLanguage> GetAllCodeLanguages();
+
+        void AddCodeLanguage(ICodeLanguage dto);
+
+        void DeleteCodeLanguage(ICodeLanguage dto);
+
+        void EditCodeLanguage(ICodeLanguage dto);
+
+        ICodeLanguage GetCodeLanguage(int id);
+    }
+}
---
+++ BLL/Services/CodeLanguageService.cs
@@ -0,0 +1,45 @@
+﻿using Abstraction.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Services
+{
+    public class CodeLanguageService : ICodeLangaugeService
+    {
+        ICodeLanguageRepo _codeLanguageRepo;
+
+        public CodeLanguageService()
+        {
+            // Inversion of Control is used to solve the dependency of the repository
+            _codeLanguageRepo = new DAL.Repository.CodeLanguageRepo();
+        }
+
+        public void AddCodeLanguage(ICodeLanguage dto)
+        {
+            _codeLanguageRepo.AddCodeLanguage(dto);
+        }
+
+        public void DeleteCodeLanguage(ICodeLanguage dto)
+        {
+            _codeLanguageRepo.DeleteCodeLanguage(dto);
+        }
+
+        public void EditCodeLanguage(ICodeLanguage dto)
+        {
+            _codeLanguageRepo.EditCodeLanguage(dto);
+        }
+
+        public List<ICodeLanguage> GetAllCodeLanguages()
+        {
+            return _codeLanguageRepo.GetAllCodeLanguages();
+        }
+
+        public ICodeLanguage GetCodeLanguage(int id)
+        {
+            return _codeLanguageRepo.GetCodeLanguage(id);
+        }
+    }
+}
