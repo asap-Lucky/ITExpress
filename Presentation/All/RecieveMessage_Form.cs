520fb7ea38946ec62fee45e0d665cea720bfa191
vaniljo <118176353+vaniljo@users.noreply.github.com>
2023-05-25 13:39:01 +0200

    Made the ReciveMesage from

--- Presentation/All/RecieveMessage_Form.cs
+++ Presentation/All/RecieveMessage_Form.cs
@@ -1,4 +1,5 @@
 ﻿using Abstraction.Interfaces;
+using BLL.Singleton;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -16,12 +17,50 @@ namespace Presentation.All
         Abstraction.Interfaces.IMessage selectedMessage;
 
         BLL.Services.MessageService MessageService;
-
+        private ICustomer loggedInCustomer;
+        private IConsultant loggedInConsultant;
         public RecieveMessage_Form(IMessage selectedMessage)
         {
             InitializeComponent();
             this.selectedMessage = selectedMessage;
-            MessageService = new BLL.Services.MessageService();
+            loggedInCustomer = CustomerSingleton.Instance().User;
+            loggedInConsultant = ConsultantSingleton.Instance().User;
+            DisplayMessageInfo();
+        }
+
+        private void DisplayMessageInfo()
+        {
+            tb_Header.Text = selectedMessage.Header;
+            tb_Body.Text = selectedMessage.Body;
+
+            if (selectedMessage.Customer != null && selectedMessage.Consultant != null)
+            {
+                // Determine the sender and receiver based on the logged-in user
+                string senderEmail;
+                string receiverEmail;
+
+                if (loggedInCustomer != null && selectedMessage.Customer.Id == loggedInCustomer.Id)
+                {
+                    // Logged-in user is the customer, assign customer as sender and consultant as receiver
+                    senderEmail = loggedInCustomer.Email;
+                    receiverEmail = selectedMessage.Consultant.Email;
+                }
+                else if (loggedInConsultant != null && selectedMessage.Consultant.Id == loggedInConsultant.Id)
+                {
+                    // Logged-in user is the consultant, assign consultant as sender and customer as receiver
+                    senderEmail = loggedInConsultant.Email;
+                    receiverEmail = selectedMessage.Customer.Email;
+                }
+                else
+                {
+                    // No logged-in user found, display empty sender and receiver
+                    senderEmail = "";
+                    receiverEmail = "";
+                }
+
+                tb_Reciver.Text = receiverEmail;
+                tb_Sender.Text = senderEmail;
+            }
         }
     }
 }
